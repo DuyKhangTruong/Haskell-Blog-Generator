fca7a17a2df123fda70fd4800b7bb9ab0a274c77
Khang Nguyen <khang@i.inc>
2025-04-26 19:58:45 -0700

    Upload init files

---
+++ main.hs
@@ -0,0 +1,13 @@
+html_  content = "<html>" <> content <> "</html>"
+
+body_ content = "<body>" <> content <> "</body>"
+
+head_ content = "<head>" <> content <> "</head>"
+
+title_ content = "<title>" <> content <> "</title>"
+
+makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+myHtml = makeHtml "Hello Title" "Body Title"
+
+main =
+    putStrLn myHtml

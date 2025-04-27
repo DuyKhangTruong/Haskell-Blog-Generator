ecdc71910013e57f414564da1814d1b0c050c0f1
Khang Nguyen <khang@i.inc>
2025-04-26 20:00:51 -0700

    Update main.hs

--- main.hs
+++ main.hs
@@ -7,7 +7,7 @@ head_ content = "<head>" <> content <> "</head>"
 title_ content = "<title>" <> content <> "</title>"
 
 makeHtml title content = html_ (head_ (title_ title) <> body_ content)
-myHtml = makeHtml "Hello Title" "Body Title"
+myHtml = makeHtml "Hello Title Testing the github" "Body Title"
 
 main =
     putStrLn myHtml
